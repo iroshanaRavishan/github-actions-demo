373b8c4819e8800247c8cf4ddb103e64c0207036
IroshanaRavishan <bandarairoshana@gmail.com>
2025-06-15 22:10:01 +0530

    adding fullly corrected code file - CORRECT FILE

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js with error
 const greet = () => {
   console.log('Hello, this code is with errors!'); // error in this line: missing semicolon 
-}
+};
 
 greet(); // error in this line: missing semicolon
